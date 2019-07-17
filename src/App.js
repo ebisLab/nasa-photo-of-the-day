f25685ecee4b712296fa812712231eb305aed50b
ebisLab <ebaiden@techie.com>
2019-07-17 19:17:14 -0400

    set up a basic template

--- src/App.js
+++ src/App.js
@@ -4,10 +4,17 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+      <section>
+        <li>Stats:</li>
+        <li>Predictions: </li>
+        <li>Images: </li>
+      </section>
+    </div>
     </div>
   );
 }
